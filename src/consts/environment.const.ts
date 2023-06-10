48cc156cd7b2f1842ec3a817831a224eabfbaae1
Laura Izabel <lauraizabel8569@gmail.com>
2023-06-10 20:31:07 -0300

    chore: typeorm config

---
+++ src/consts/environment.const.ts
@@ -0,0 +1,9 @@
+export const environment = {
+  database: {
+    host: process.env.DATABASE_HOST || 'localhost',
+    port: Number(process.env.DATABASE_PORT) || 5432,
+    username: process.env.DATABASE_USERNAME || 'postgres',
+    password: process.env.DATABASE_PASSWORD || '1234',
+    name: process.env.DATABASE_NAME || 'postgres',
+  },
+};
