43a4921110f9bb88edf383e0ef5fc19b2e611c5e
Laura Izabel <lauraizabel8569@gmail.com>
2023-08-29 22:13:55 -0300

    chore: update swagger

--- src/main.ts
+++ src/main.ts
@@ -6,9 +6,21 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
 import * as bodyParser from 'body-parser';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  const config = new DocumentBuilder()
+    .setTitle('Fureverhome')
+    .setDescription('The Fureverhome API description')
+    .setVersion('1.0')
+    .addTag('fureverhome')
+    .build();
+
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
+
   app.use(helmet());
   app.useGlobalPipes(
     new ValidationPipe({
